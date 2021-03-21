bcedd714795ff9522767768ff75e78bc17af4281
isiah curtis emmanuel <isiah.emmanuel@outlook.com>
2021-03-21 15:41:12 -0400

    changes to logo on navbar
    styling changes

--- myjs.js
+++ myjs.js
@@ -4,9 +4,9 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("logo").style.fontSize = "10px";
   } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("logo").style.fontSize = "15px";
   }
 }
